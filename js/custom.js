b81fca1294133ad0b441400b383f0b908fefff39
Ian Scrivener <github@zilogy.asia>
2014-02-16 10:50:12 +1100

    upd

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 	// prettyPhoto
 	$("a[class^='prettyPhoto']").prettyPhoto();
@@ -40,46 +39,3 @@ $(document).ready(function(){
         });
       });
 });
-=======
-$(document).ready(function(){
-	// prettyPhoto
-	$("a[class^='prettyPhoto']").prettyPhoto();
-
-	// plugin carousel
-	$('.carousel').carousel();
-
-	// jQuery onePageNav 
-	$('.navbar-nav').onePageNav({
-		currentClass: 'active',
-		changeHash: false,
-		scrollSpeed: 750,
-		scrollOffset: 94,
-		scrollThreshold: 0.1,
-		easing: 'swing',
-	});
-
-	// jquery plugin cycle
-	$('.list-testimonial').cycle({
-		slides: 'li',
-		pager: ".testimonial-bullets",
-		timeout: 0,
-		autoHeight: 0,
-		centerHorz: true
-	});
-
-	// size testimonial 
-	$('.list-testimonial').on( 'cycle-update-view', function(e,o,sh,cs) {
-		var $this = $(this);
-
-        $this.animate({
-          height: $(cs).height()
-        }, 500);
-
-        $(window).resize(function() {
-          $this.stop().animate({
-            height: $(cs).height()
-          }, 500);
-        });
-      });
-});
->>>>>>> e961b0125a5a20748b025ab77e2aab2fbb3bb087
